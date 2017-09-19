e95fe7e401acedb1df0ccbe26f3381aaabc33b8e
gongalili <gon.galili5@gmail.com>
2017-09-19 14:10:11 +0300

    66
    666

---
+++ app/src/main/java/com/example/madaim/test111/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.madaim.test111;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ///yiuyiuyyyytrtt
+        //rtuyutuj
+        int x = 8;
+    }
+}
